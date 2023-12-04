8773346d1f7736e540cff99716d52747214adfdb
Hezron Nyamboga <hezzyn1@gmail.com>
2023-12-04 20:08:26 +0300

    intial commit

---
+++ number_guesser.py
@@ -0,0 +1,37 @@
+import random
+
+top_of_range = input("Type a number:  ")
+
+if top_of_range.isdigit():
+    top_of_range = int(top_of_range)
+
+    if top_of_range <= 0:
+        print("Please enter a lager number next time.")
+        quit()
+
+else:
+    print("Please enter a number next time")
+    quit()
+
+random_number = random.randint(0, top_of_range)
+
+guesses = 0
+
+while True:
+    guesses += 1
+    user_guess = input("make a guess: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print("Please enter a number next time.")
+        continue
+
+    if user_guess == random_number:
+        print("Correct Guess!")
+        break
+    elif user_guess > random_number:
+        print("You were above the number!")
+    else:
+        print("You were below the number!")
+
+print("You got it in", guesses, "guesses")
